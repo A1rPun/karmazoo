7e5a704cd61a6ec90f12a5f09546766cae2626af
A1rPun <thombakker@gmail.com>
2025-04-09 21:32:59 +0200

    Implement titles

--- src/app/zoo.interface.ts
+++ src/app/zoo.interface.ts
@@ -1,9 +1,12 @@
 export interface IZoo {
-  name: string,
-  price?: number,
-  text?: string,
-  secret?: boolean,
-  size?: number,
-  star?: string,
-  state?: number,
+  name: string
+  price?: number
+  text?: string
+  secret?: boolean
+  size?: number
+  star?: string
+  state?: number
+  mission2?: string
+  mission3?: string
+  unlockedText?: string
 }
